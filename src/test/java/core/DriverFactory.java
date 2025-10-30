96e731b51827eaa5c6a090304212c4a0410d7c0e
Victor Hugo <victorhpdantas@gmail.com>
2025-10-29 22:01:49 -0300

    adicionando chorome options

--- src/test/java/core/DriverFactory.java
+++ src/test/java/core/DriverFactory.java
@@ -3,6 +3,7 @@ package core;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
     private static WebDriver driver;
@@ -12,7 +13,16 @@ public class DriverFactory {
     public static WebDriver getDriver() {
         if (driver == null) {
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+            //chorome options
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+            options.addArguments("--remote-allow-origins=*");
+
+            driver = new ChromeDriver(options);
         }
         return driver;
     }
--- src/test/java/pageObjects/CadastroPO.java
+++ src/test/java/pageObjects/CadastroPO.java
@@ -1,14 +1,10 @@
 package pageObjects;
 
 import core.DSL;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 
 public class CadastroPO {
     private DSL dsl;
